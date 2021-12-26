e86455ce0dd32ec1126032f92ebf0192a7f4cdee
nafisK <nafisrizwank@gmail.com>
2021-12-26 13:22:12 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/uiux_calculator/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.uiux_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: android.view.View) {
+        Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
+
+
+    }
+}
