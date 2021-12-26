6428daaafb53cfc7eeeb84c7d9b66234154fe2df
nafisK <nafisrizwank@gmail.com>
2021-12-26 13:57:25 -0500

    ui changes & some operator checks

--- app/src/main/java/com/example/uiux_calculator/MainActivity.kt
+++ app/src/main/java/com/example/uiux_calculator/MainActivity.kt
@@ -2,17 +2,68 @@ package com.example.uiux_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
+
+
     }
 
     fun onDigit(view: android.view.View) {
-        Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show()
-
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+        lastDot = false
 
     }
+
+    fun onCLear(view: View) {
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view: android.view.View) {
+        if (lastNumeric && !lastDot) {
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+
+
+        }
+    }
+
+    fun onOperator(view: View) {
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = true
+            }
+
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains('/')
+                    || value.contains('*')
+                    || value.contains('+')
+                    || value.contains('-')
+        }
+    }
+
+
 }
